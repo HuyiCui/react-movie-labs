fc3c83702c262ee7c702bcaed541899a99dbf1e8
HuyiCui <cuihuyi512@gmail.com>
2024-11-16 19:46:55 +0000

    Added Now Playing movies page.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -87,12 +87,25 @@ export const getMovie = (args) => {
    });
   };
 
-  export const getUpcomingMovies = (args) => {
-    console.log(args)
-    const [, idPart] = args.queryKey;
-    const { id } = idPart;
+  export const getUpcomingMovies = () => {
     return fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+
+  export const getNowPlayingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
     ).then((response) => {
       if (!response.ok) {
         return response.json().then((error) => {
--- movies/src/index.js
+++ movies/src/index.js
@@ -11,6 +11,7 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
 import UpcomingMoviesPage from './pages/upcomingMoviesPage'
+import NowPlayingPage from "./pages/nowPlayingPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -36,7 +37,7 @@ const App = () => {
             <Route path="*" element={ <Navigate to="/" /> } />
             <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
             <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-            <Route path="movies/mustWatch" element={<mustWatchPage />} />
+            <Route path="/movies/nowplaying" element={<NowPlayingPage />} />
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
